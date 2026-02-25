c6da2c4b05175f5433a656065d7870e985d50772
Lucas Verrecchia <lucas.verrecchia@gmail.com>
2026-02-25 09:23:00 +0100

    Initial project setup, Spring Boot 4, pom.xml dependencies

---
+++ src/test/java/fr/atex/bibliotheque/BibliothequeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.atex.bibliotheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliothequeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
+
